cc8e3b751c3006f4c9b8b355989b9248653377ad
AzretRealme <azretovichh@gmail.com>
2020-10-03 00:43:12 +0600

    ignore git

--- src/newrep.java
+++ src/newrep.java
@@ -1,7 +1,7 @@
 public class newrep {
-    //now do
-    //пока сильна Джава , будет развиваться С++
+    public static void main(String[] args){
+        System.out.println("IGNORE");
+    }
 
-    //javaaa gosling
 
 }
